8428bda9675cb175dfe6f819463db9b02e573ff4
wendhil20 <wendhil10@gmail.com>
2026-07-17 16:04:35 +0800

    optimize

--- link/top.php
+++ link/top.php
@@ -8,12 +8,12 @@ if (!defined('ROOT_PATH')) {
 define('QR_SECRET', 'warehouse_secret_2024');
 ?>
 
-<!-- link this page to cdn-->
-<link rel="stylesheet" href="<?= BASE_URL ?>/link/css/tailwind.min.css">
-
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
-
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@400;500;600;700&display=swap" rel="stylesheet" />
+
+<!-- Sarili mong pinag-build na Tailwind CSS, hindi CDN script -->
+<link rel="stylesheet" href="<?= BASE_URL ?>/assets/css/tailwind.min.css">
 
 <link rel="icon" type="image/png" href="<?= BASE_URL ?>/icon/logo.png">
 
